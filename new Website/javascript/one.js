c8b918202f24c234663edabbb7208867a5038346
SamZayd <sadafk2304@gmail.com>
2021-12-09 13:48:18 +0530

    done

---
+++ new Website/javascript/one.js
@@ -0,0 +1,11 @@
+var no = 123
+var name = "Sam"
+var message = "This is wierd"
+var details = {F_name : "Sadaf" , l_name : "Khan"}
+var mul_no = [20,30,40]
+
+console.log(typeof no)
+console.log(typeof name)
+console.log(typeof message)
+console.log(typeof details)
+console.log(typeof mul_no)
